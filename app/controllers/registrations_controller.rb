cdae2bfec356697d4209c6387e291ee84791b5c7
j.taneichi <j.tane3ma@gmail.com>
2014-12-03 20:26:14 +0900

    update controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,6 +5,15 @@ class RegistrationsController < ApplicationController
 
   def create
     # raise Exception.new(params[:user])
-    redirect_to new_registration_path, flash: {debug: params[:user]}
+    @user = User.new(params_user)
+    if @user.save
+      redirect_to new_registration_path, flash: {debug: "collect!->" + params[:user].to_s}
+    else
+      render :new, flash: {debug: "false"}
+    end
+  end
+
+  def params_user
+    params.require(:user).permit(:name, :email, :screen_name, :bio, :password, :password_confirmation)
   end
 end
