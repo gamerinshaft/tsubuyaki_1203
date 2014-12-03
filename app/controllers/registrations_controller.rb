86120aadd4862ca740d2a49d0433d40b5664f25d
j.taneichi <j.tane3ma@gmail.com>
2014-12-03 19:42:54 +0900

    edit submit form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,4 +2,9 @@ class RegistrationsController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    # raise Exception.new(params[:user])
+    redirect_to new_registration_path
+  end
 end
